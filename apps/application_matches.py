4b73e11f110018b86351b133100c48c7b129b17e
FidgetingBits <fidgetingbits@users.noreply.github.com>
2024-02-19 18:40:41 +0800

    feat: Add app matches in single place where file isn't needed

--- apps/application_matches.py
+++ apps/application_matches.py
@@ -1,11 +1,29 @@
 from talon import Module
 
 mod = Module()
-
-
 apps = mod.apps
 
 apps.gdb = """
 win.title: /TERM:gdb/
 and win.title: /MODE:t/
+"""
+
+# NOTE: Double \\ to avoid re.error bad escape character \c
+apps.cmd = """
+win.title: /TERM:C:\\\\Windows\\\\system32\\\\cmd.exe/
+"""
+
+# FIXME: This is not generic as MINGW64 is a compiler, not actually git_bash specific
+apps.git_bash = """
+win.title: /TERM:MINGW64/
+"""
+
+apps.talon_repl = """
+win.title: /TERM:Talon - REPL/
+win.title: /TERM:~/.talon/bin/repl/
+"""
+
+# NOTE: Assumes something like: PS1="\[\e]0;${debian_chroot:+($debian_chroot)}wsl: \w\a\]$PS1"
+apps.wsl = """
+win.title: /TERM:wsl/
 """
