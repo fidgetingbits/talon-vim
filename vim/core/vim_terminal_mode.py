4e46700ba9189b2624be3eda869677efd27127d6
fidgetingbits <fidgetingbits@users.noreply.github.com>
2024-12-11 16:59:32 +0800

    feat: make terminal command parsing more robust

--- vim/core/vim_terminal_mode.py
+++ vim/core/vim_terminal_mode.py
@@ -22,35 +22,27 @@ def parse_vim_term_title(window):
     """
     global last_window
     global last_title
-    # start = timer()
 
     current_title = window.title
     if last_window == window and last_title == current_title:
-        # print("parse_vim_term_title(): Skipping due to duplicate title")
-        # end = timer()
-
-        #        print(start)
-        #        print(end)
-        # print(f"parse_vim_term_title - duplicate: {end - start}")
         return
     if (
         window != ui.active_window()
         or not current_title.startswith("VIM MODE:t")
         or "TERM:" not in current_title
     ):
-        # print("parse_vim_term_title(): Skipping due to not a terminal")
-        # end = timer()
-        #        print(start)
-        #        print(end)
-        #        print(f"parse_vim_term_title - not vim: {end - start}")
-
         return
 
     last_window = window
     last_title = current_title
 
-    # pull a TERM: line out of something potentially like
+    # The goal is to try to workout the shell command being run in the vim terminal, so we
+    # pull a TERM: line out of a vim title with a format similar to:
     # VIM MODE:t RPC:/tmp/nvimlVeccr/0  TERM:gdb (term://~//161917:/usr/bin/zsh) zsh
+    # This has lots of potential edge cases though depending on operating system, terminal, etc:
+    #  - sudo gdb
+    #  - sudo nix-shell -p gdb.out --run 'gdb -p 1252'
+    #  - nix shell nixpkgs#gdb -c 'gdb'
     index = current_title.find("TERM:")
     shell_command = current_title[index + len("TERM:") :]
     cwd = None
@@ -58,23 +50,39 @@ def parse_vim_term_title(window):
         # FIXME: This will break if there's a : or ( in the path
         cwd = shell_command.split(":")[1].split("(")[0]
         shell_command = shell_command.split(":")[0]
+
+    # strip sudo to start
     if shell_command.startswith("sudo"):
-        # Handle something like:
-        # VIM MODE:t RPC:/tmp/nvimlVeccr/0  TERM:sudo gdb (term://~//161917:/usr/bin/zsh) zsh
-        shell_command = shell_command.split(" ")[1]
+        shell_command = " ".join(shell_command.split(" ")[1:])
+
+    # see if it's some complex invocation
+    if "nix-shell" in shell_command:
+        if "--run " in shell_command:
+            shell_command = shell_command.split("--run ")[1].split(" ")[0]
+        else:
+            print(f"WARNING: unable to infer command from nix-shell: {shell_command}")
+    elif "nix shell" in shell_command:
+        if "-c " in shell_command:
+            shell_command = shell_command.split("-c ")[1].split(" ")[0]
+        else:
+            print(f"WARNING: unable to infer command from nix shell: {shell_command}")
+    # naively just take the first word
     else:
         shell_command = shell_command.split(" ")[0]
-    # end = timer()
-    #    print(start)
-    #    print(end)
-    #    print(f"parse_vim_term_title - pass to populate: {end - start}")
+
+    # depending on the above, our command may be prefixed with some unwanted stuff, so we strip it
+    for c in ["'", '"']:
+        shell_command = shell_command.replace(c, "")
 
     tags = populate_shell_tags(shell_command, window.title)
     # if tags is not None and "terminal" in tags:
     #     # Parse folder from the window title
     #     cwd = window.title.split("TERM:")[1].split(":")[1].split("(term")[0]
 
-    populate_language_modes(shell_command)
+    # FIXME: This needs to switch to using the new action for forced language, and having the app itself override the
+    # action to specify whatever the language should be rather than have VIM trying to special case it. VIM should just
+    # ensure that the app context is active (if even needed beyond title matching)
+    # populate_language_modes(shell_command)
 
 
 language_specific_commands = {
@@ -82,7 +90,7 @@ language_specific_commands = {
 }
 
 
-def populate_language_modes(shell_command):
+def populate_language_modes():
     """Tries to enable language modes based off special cases of programs being
     run, for instance running debuggers to enable gdb language, or running
     repl to enable python language
@@ -93,6 +101,7 @@ def populate_language_modes(shell_command):
     for command in language_specific_commands.keys():
         if shell_command.endswith(command):
             print("vim_terminal.py: setting context-specific language")
+            # FIXME: WARNING this no longer exists... see comment for populate_language_modes calln
             actions.user.code_set_context_language(language_specific_commands[command])
             return
 
@@ -120,15 +129,16 @@ regex_shell_tags = {
 }
 
 shell_tags = {
-    # "zsh": "terminal",
+    "zsh": "terminal",
     # "bash": "terminal",
     # "sh": "terminal",
     "ssh": "terminal",
     "sudo": "terminal",
     "edit": "terminal",  # This matches when edit is hung waiting for vscode
-    # "gdb": "user.gdb",
-    # "pwndbg": ["user.gdb", "user.pwndbg"],
-    # "gef": ["user.gdb", "user.gef"],
+    # FIXME: I had disabled these in favor of a separate method, but re enabling them do to nix quirks
+    "gdb": "user.gdb",
+    "pwndbg": ["user.gdb", "user.pwndbg"],
+    "gef": ["user.gdb", "user.gef"],
     # "htop": "user.htop",
     "taskwarrior-tui": "user.taskwarrior_tui",
     "~/.talon/bin/repl": "user.talon_repl",
@@ -139,34 +149,27 @@ shell_tags = {
 
 
 def populate_shell_tags(shell_command, window_title):
-    """TODO: Docstring for populate_shell_tags.
-    :returns: TODO
+    """Set a context tag for the shell command shell_command extracted from window_title
+    :returns: A list of tags set
 
     """
 
     tags = []
+    print(f"populate_shell_tags: command: {shell_command}")
+    # print(shell_tags)
     if shell_command in shell_tags:
-        # XXX - make a better way of marking stuff in noisy event log,
-        # ins this writes to our file/terminal
-        # actions.insert('marker start')
-        # print(f"setting shell tags: {window_title}")
-        # print(ctx.tags)
+        print(f"populate_shell_tags: setting shell tags: {window_title}")
+        print(f"current context tags: {ctx.tags}")
         if isinstance(shell_tags[shell_command], list):
-            start = timer()
             tags = shell_tags[shell_command]
             ctx.tags = tags
         else:
-            start = timer()
             tags = [shell_tags[shell_command]]
             ctx.tags = tags
-        end = timer()
-        # print(f"populate_shell_tags: {end - start}")
 
-        # actions.insert('marker end')
-
-        # print(f"populate_shell_tags(): set {ctx.tags}")
+        print(f"populate_shell_tags(): set {ctx.tags}")
     else:
-        # print(f"trying fuzzy: {window_title}")
+        print(f"populate_shell_tags: trying fuzzy: {window_title}")
         found_fuzzy = False
         for tag in fuzzy_shell_tags:
             # print(f"shell_command: {shell_command}")
